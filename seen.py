adb8a6be81462d90851b25ef3031660756109302
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-07-28 18:02:09 +0100

    Structure

---
+++ seen.py
@@ -0,0 +1,9 @@
+from opsdroid.skills import match_regex
+import logging
+
+def setup(opsdroid):
+    logging.debug("Loaded seen module")
+
+@match_regex(r'when did you last see (.*)\?')
+def last_seen(opsdroid, message):
+    message.respond("Never seen them before")

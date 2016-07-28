2c2fa73a2696b8ff691bba3d0b4f8c0272662812
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-07-28 18:36:56 +0100

    Added functionality

--- seen.py
+++ seen.py
@@ -1,4 +1,5 @@
 from opsdroid.skills import match_regex
+from datetime import datetime
 import logging
 
 def setup(opsdroid):
@@ -6,4 +7,17 @@ def setup(opsdroid):
 
 @match_regex(r'when did you last see (.*)\?')
 def last_seen(opsdroid, message):
-    message.respond("Never seen " + message.regex.group(1) + " before")
+    name = message.regex.group(1)
+    seen = opsdroid.memory.get("seen")
+    if seen != None or name not in seen:
+        message.respond("Never seen " + message.regex.group(1) + " before")
+    else:
+        message.respond("I last saw " + message.regex.group(1) + " on " + seen[name])
+
+@match_regex(r'.*')
+def update_seen(opsdroid, message):
+    seen = opsdroid.memory.get("seen")
+    if seen = None:
+        seen = {}
+    seen[message.user] = datetime.now()
+    opsdroid.memory.put("seen", seen)

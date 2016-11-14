a6b78eaee45d4f49e31782f3f1b66aaf7e6260fc
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-14 14:56:35 +0000

    Added async

--- seen.py
+++ seen.py
@@ -7,18 +7,18 @@ def setup(opsdroid):
     logging.debug("Loaded seen module")
 
 @match_regex(r'when did you last see (.*)\?')
-def last_seen(opsdroid, message):
+async def last_seen(opsdroid, message):
     name = message.regex.group(1)
-    seen = opsdroid.memory.get("seen")
+    seen = await opsdroid.memory.get("seen")
     if seen == None or name not in seen:
-        message.respond("I've never seen " + message.regex.group(1) + " before")
+        await message.respond("I've never seen " + message.regex.group(1) + " before")
     else:
-        message.respond("I last saw " + message.regex.group(1) + " " + human(seen[name], precision=1))
+        await message.respond("I last saw " + message.regex.group(1) + " " + human(seen[name], precision=1))
 
 @match_regex(r'.*')
-def update_seen(opsdroid, message):
-    seen = opsdroid.memory.get("seen")
+async def update_seen(opsdroid, message):
+    seen = await opsdroid.memory.get("seen")
     if seen == None:
         seen = {}
     seen[message.user] = datetime.now()
-    opsdroid.memory.put("seen", seen)
+    await opsdroid.memory.put("seen", seen)

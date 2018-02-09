21157a8a1cf22c9caade69ac9bc8f1551c3008d3
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2018-02-09 15:25:24 +0000

    Update regex

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from opsdroid.matchers import match_regex
+from opsdroid.matchers import match_regex, match_always
 from datetime import datetime
 from ago import human
 import logging
@@ -6,16 +6,16 @@ import logging
 def setup(opsdroid):
     logging.debug("Loaded seen module")
 
-@match_regex(r'when did you last see (.*)\?')
+@match_regex(r'when did you last see (?P<name>.*)\?')
 async def last_seen(opsdroid, config, message):
-    name = message.regex.group(1)
+    name = message.regex.group('name')
     seen = await opsdroid.memory.get("seen")
     if seen == None or name not in seen:
-        await message.respond("I've never seen " + message.regex.group(1) + " before")
+        await message.respond("I've never seen {} before".format(name))
     else:
-        await message.respond("I last saw " + message.regex.group(1) + " " + human(seen[name], precision=1))
+        await message.respond("I last saw {} {}".format(name, human(seen[name], precision=1)))
 
-@match_regex(r'.*')
+@match_always
 async def update_seen(opsdroid, config, message):
     seen = await opsdroid.memory.get("seen")
     if seen == None:
